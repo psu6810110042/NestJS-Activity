995474c2226693cf4d4d62ef303d3c494fefca99
Jirakorn Sukmee <6810110042@psu.ac.th>
2025-12-17 04:31:21 +0000

    added book entity and like functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookCategoryModule } from './book-category/book-category.module';
 import { BookCategory } from './book-category/entities/book-category.entity';
+import { BookModule } from './book/book.module';
+import { Book } from './book/entities/book.entity';
 
 @Module({
   imports: [
@@ -12,10 +14,11 @@ import { BookCategory } from './book-category/entities/book-category.entity';
       username: 'admin',
       password: 'password123',
       database: 'bookstore_dev',
-      entities: [BookCategory], // เราจะเพิ่ม Entities ที่นี่ในภายหลัง
+      entities: [Book,BookCategory], // เราจะเพิ่ม Entities ที่นี่ในภายหลัง
       synchronize: true, // สร้าง Table อัตโนมัติ (ใช้สำหรับ Dev เท่านั้น)
     }),
     BookCategoryModule,
+    BookModule,
   ],
 })
 export class AppModule {}
---
+++ src/book/book.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class BookService {
+  constructor(
+    @InjectRepository(Book)
+    private readonly repo: Repository<Book>,
+  ) {}
+
+  create(createBookDto: CreateBookDto) {
+    return this.repo.save(createBookDto);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  async findOne(id: string): Promise<Book> {
+    const book = await this.repo.findOneBy({ id });
+    if (!book) {
+      throw new NotFoundException(`Book with ID ${id} not found`);
+    }
+    return book;
+  }
+
+  update(id: string, updateBookDto: UpdateBookDto) {
+    return this.repo.update(id, updateBookDto);
+  }
+
+  remove(id: string) {
+    return this.repo.delete(id);
+  }
+
+  async incrementLikes(id: string) {
+    const book = await this.findOne(id);
+    book.likeCount += 1;
+    return this.repo.save(book);
+  }
+}
