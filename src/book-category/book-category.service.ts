8e1cca8f74a9b201ab91b354385e03f3b9586d0c
Jirakorn Sukmee <6810110042@psu.ac.th>
2025-12-17 03:12:11 +0000

    checkpoint 2

--- src/book-category/book-category.service.ts
+++ src/book-category/book-category.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookCategoryDto } from './dto/create-book-category.dto';
 import { UpdateBookCategoryDto } from './dto/update-book-category.dto';
+import { BookCategory } from './entities/book-category.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class BookCategoryService {
+  constructor(
+    @InjectRepository(BookCategory)
+    private readonly repo: Repository<BookCategory>,
+  ) {}
+
+  async onModuleInit() {
+    const count = await this.repo.count();
+    if (count === 0) {
+      console.log('Seeding Book Categories...');
+      await this.repo.save([
+        { name: 'Fiction', description: 'Stories and novels' },
+        { name: 'Technology', description: 'Computers and engineering' },
+        { name: 'History', description: 'Past events' }
+      ]);
+    }
+  }
   create(createBookCategoryDto: CreateBookCategoryDto) {
     return 'This action adds a new bookCategory';
   }
