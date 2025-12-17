8e84faf7ef20cf0c4276a16db46ca726c5a9bd84
Jirakorn Sukmee <6810110042@psu.ac.th>
2025-12-17 03:58:42 +0000

    added POST PATCH DELETE functionality and checkpoint 3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  // await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
