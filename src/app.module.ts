22a5fa05e2eb4b7a4601d4a686d7b4bf2de65a21
Jirakorn Sukmee <6810110042@psu.ac.th>
2025-12-19 02:58:51 +0000

    created user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,31 @@ import { BookCategoryModule } from './book-category/book-category.module';
 import { BookCategory } from './book-category/entities/book-category.entity';
 import { BookModule } from './book/book.module';
 import { Book } from './book/entities/book.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],      // บอกว่า Module นี้ต้องใช้ ConfigModule
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'), // ใช้ ConfigService ดึงค่า แทน process.env
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],      // Inject ConfigService เข้ามาใน Factory
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -14,11 +36,12 @@ import { Book } from './book/entities/book.entity';
       username: 'admin',
       password: 'password123',
       database: 'bookstore_dev',
-      entities: [Book,BookCategory],
+      entities: [Book, BookCategory],
       synchronize: true, // สร้าง Table อัตโนมัติ (ใช้สำหรับ Dev เท่านั้น)
     }),
     BookCategoryModule,
     BookModule,
+    UsersModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength, IsOptional, IsEnum } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+
+  @IsEnum(UserRole)
+  @IsOptional()
+  role?: UserRole;
+}
